ee9e0411c1c76a217338f3c8d65661a48b072e44
tvgDev <thiago.guimaraaes.dev@gmail.com>
2025-09-08 23:55:38 -0300

    add: Toastify integration and validations on code.
    Adicionei validações para erros, com integração com o Toastify para melhor visualização.

--- password-generator/script.js
+++ password-generator/script.js
@@ -26,7 +26,8 @@ function getPasswordSize() {
   const size = document.querySelector("#size").value;
 
   if (isNaN(size) || size < 4 || size > 128) {
-    alert("Tamanho inválido, Digite um número entre 4 e 128!");
+    message("Tamanho inválido, digite um número entre 4 e 128", "warning");
+    return null;
   }
 
   return size;
@@ -40,22 +41,61 @@ function randomCharType(ChartTypes) {
   ];
 }
 
-function generatePassword(size, ChartTypes) {
+function generatePassword(size, charTypes) {
   let passwordGenerated = "";
+  const selectedChars = charTypes.join("");
+
+  charTypes.forEach((type) => {
+    passwordGenerated += type[Math.floor(Math.random() * type.length)];
+  });
 
   while (passwordGenerated.length < size) {
-    passwordGenerated += randomCharType(ChartTypes);
+    passwordGenerated +=
+      selectedChars[Math.floor(Math.random() * selectedChars.length)];
   }
 
+  passwordGenerated = passwordGenerated
+    .split("")
+    .sort(() => Math.random() - 0.5)
+    .join("");
+
   return passwordGenerated;
+}
+
+function message(text, status = "success") {
+  Toastify({
+    text: text,
+    duration: 3000,
+    style: {
+      background: status === "success" ? "#84cc16" : "#dc2626",
+      boxShadow: "none",
+    },
+  }).showToast();
 }
 
 document.querySelector("#generate").addEventListener("click", function () {
   const size = getPasswordSize();
   const ChartTypes = getChartTypes();
 
+  if (!size) {
+    return;
+  }
+
+  if (!ChartTypes.length) {
+    message("Selecione pelo menos um tipo de caractere", "danger");
+    return;
+  }
+
   const passwordGenerated = generatePassword(size, ChartTypes);
 
   document.querySelector("#password_container").classList.add("show");
   document.querySelector("#password").textContent = passwordGenerated;
+});
+
+document.querySelector("#copy").addEventListener("click", function () {
+  navigator.clipboard.writeText(
+    document.querySelector("#password").textContent
+  );
+
+  message("Senha copiada com sucesso!", "success");
 });
